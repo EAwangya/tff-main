58eda607128259e43915665d7e2c8517c595efad
Awangya, Ernest <ernest.awangya@gmail.com>
2021-06-10 20:05:11 -0600

    added

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "access_key" {
+  default = "AKIARQAORRT4EYMFQIPX"
+  }
+variable "secret_key" {
+  default = "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"
+  }
+variable "region" {
+  default = "us-east-1"
+  }
