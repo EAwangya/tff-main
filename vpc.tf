eca54b63c18b42d1ee7a6077e821c0ef55fbebf2
EAwangya <84739332+EAwangya@users.noreply.github.com>
2021-06-10 20:23:26 -0600

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,48 +1,101 @@
-resource "aws_vpc" "demo" {
-  cidr_block       = var.cidr_block[0]
-  instance_tenancy = "default"
+# Create a VPC
+
+resource "aws_vpc" "MyLab-VPC"{
+    cidr_block = var.cidr_block[0]
+
+    tags = {
+        Name = "MyLab-VPC"
+    }
+
+}
+
+# Create Subnet
+
+resource "aws_subnet" "MyLab-Subnet1" {
+    vpc_id = aws_vpc.MyLab-VPC.id
+    cidr_block = var.cidr_block[1]
+
+    tags = {
+        Name = "MyLab-Subnet1"
+    }
+}
+
+# Create Internet Gateway
+
+resource "aws_internet_gateway" "MyLab-IntGW" {
+    vpc_id = aws_vpc.MyLab-VPC.id
+
+    tags = {
+        Name = "MyLab-InternetGW"
+    }
+}
+
+
+# Create Secutity Group
+
+resource "aws_security_group" "MyLab_Sec_Group" {
+  name = "MyLab Security Group"
+  description = "To allow inbound and outbound traffic to mylab"
+  vpc_id = aws_vpc.MyLab-VPC.id
+
+  dynamic ingress {
+      iterator = port
+      for_each = var.ports
+       content {
+            from_port = port.value
+            to_port = port.value
+            protocol = "tcp"
+            cidr_blocks = ["0.0.0.0/0"]
+       }
+
+  }
+
+  egress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+  } 
 
   tags = {
-    Name = "demo-vpc"
+      Name = "allow traffic"
   }
+
 }
-resource "aws_subnet" "demo1" {
-  vpc_id     = aws_vpc.demo.id
-  cidr_block = var.cidr_block[1]
+
+# Create route table and association
+
+resource "aws_route_table" "MyLab_RouteTable" {
+    vpc_id = aws_vpc.MyLab-VPC.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.MyLab-IntGW.id
+    }
+
+    tags = {
+        Name = "MyLab_Routetable"
+    }
+}
+
+# Create route table association
+resource "aws_route_table_association" "MyLab_Assn" {
+    subnet_id = aws_subnet.MyLab-Subnet1.id
+    route_table_id = aws_route_table.MyLab_RouteTable.id
+}
 
 
+# Create an AWS EC2 Instance
+
+resource "aws_instance" "DemoResource" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  key_name = "nvkp"
+  vpc_security_group_ids = [aws_security_group.MyLab_Sec_Group.id]
+  subnet_id = aws_subnet.MyLab-Subnet1.id
+  associate_public_ip_address = true
+  
   tags = {
-    Name = "subnet1"
+    Name = "DemoResource"
   }
 }
-resource "aws_subnet" "demo2" {
-  vpc_id     = aws_vpc.demo.id
-  cidr_block = var.cidr_block[2]
-
-
-  tags = {
-    Name = "subnet2"
-  }
-}
-resource "aws_internet_gateway" "massgw" {
-  vpc_id = aws_vpc.demo.id
-
-  tags = {
-    Name = "mass"
-  }
-}
-resource "aws_route_table" "Pu_RT" {
-  vpc_id = aws_vpc.demo.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.massgw.id
-  }
-
-  tags = {
-    Name = "massian"
-  }
-}
-resource "aws_route_table" "Private-RT" {
-  vpc_id = aws_vpc.demo.id
-  }
